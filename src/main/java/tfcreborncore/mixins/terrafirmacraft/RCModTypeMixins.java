d18621e5dcd03dfd40d62d791a353521c3d4f3cd
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-03-23 13:15:14 +0000

    Fix mixin replacing all sounds with slime sound xD

--- src/main/java/tfcreborncore/mixins/terrafirmacraft/RCModTypeMixins.java
+++ src/main/java/tfcreborncore/mixins/terrafirmacraft/RCModTypeMixins.java
@@ -2,11 +2,14 @@ package tfcreborncore.mixins.terrafirmacraft;
 
 import net.minecraft.block.SoundType;
 
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+
 
 import mrthomas20121.tfc_decoration.api.ModTypes;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(ModTypes.class)
 public class RCModTypeMixins {
@@ -14,16 +17,20 @@ public class RCModTypeMixins {
     /**
      * Replace only the SoundType for RAW_MUD during ModTypes.init()
      */
-    @ModifyArg(
-               method = "init",
-               at = @At(
-                        value = "INVOKE",
-                        target = "Lmrthomas20121/tfc_decoration/api/ModTypes;addRockType(Ljava/lang/String;Lnet/minecraft/block/SoundType;IZ)Lmrthomas20121/tfc_decoration/api/ModTypes$RockType;",
-                        remap = false),
-               index = 1, // the SoundType parameter
-               remap = false)
-    private static SoundType replaceRawMudSound(SoundType original) {
-        // Only override the sound for raw_mud
-        return SoundType.SLIME;
+    @ModifyArgs(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lmrthomas20121/tfc_decoration/api/ModTypes;addRockType(Ljava/lang/String;Lnet/minecraft/block/SoundType;IZ)Lmrthomas20121/tfc_decoration/api/ModTypes$RockType;",
+                    remap = false
+            ),
+            remap = false
+    )
+    private static void replaceRawMudSound(Args args) {
+        String name = args.get(0);
+
+        if ("raw_mud".equals(name)) {
+            args.set(1, SoundType.SLIME);
+        }
     }
 }

3637d3cbaba6081781f40bbc40945a9a41a9b988
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-03-25 21:49:32 +0000

    Spotless

--- src/main/java/tfcreborncore/objects/tools/ItemRCTool.java
+++ src/main/java/tfcreborncore/objects/tools/ItemRCTool.java
@@ -160,7 +160,7 @@ public class ItemRCTool extends ItemTFC implements IMetalItem {
         for (BlockPos.MutableBlockPos extraPos : BlockPos.getAllInBoxMutable(minX, minY, minZ, maxX, maxY, maxZ)) {
 
             if (extraPos.equals(pos)) continue;
-            
+
             // Disable AOE without breaking functionality
             if (entityLiving.isSneaking()) {
                 stack.damageItem(1, entityLiving);

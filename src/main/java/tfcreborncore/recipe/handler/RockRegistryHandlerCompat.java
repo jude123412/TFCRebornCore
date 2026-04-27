78aed1fd67b0d6adb3bbc846050adf282bd7caa1
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-04-27 14:23:48 +0100

    spotless

--- src/main/java/tfcreborncore/recipe/handler/RockRegistryHandlerCompat.java
+++ src/main/java/tfcreborncore/recipe/handler/RockRegistryHandlerCompat.java
@@ -1,15 +1,16 @@
 package tfcreborncore.recipe.handler;
 
+import java.util.Arrays;
+import java.util.List;
+
 import net.dries007.tfc.api.registries.TFCRegistries;
 import net.dries007.tfc.api.types.Rock;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
 import tfcreborncore.recipe.ICompatModule;
 import tfcreborncore.recipe.RecipeHelper;
 import tfcreborncore.recipe.enums.Mods;
 import tfcreborncore.recipe.manager.ForestryRecipeManager;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class RockRegistryHandlerCompat implements ICompatModule {
 
@@ -17,18 +18,18 @@ public class RockRegistryHandlerCompat implements ICompatModule {
     public List<String> dependencies() {
         return Arrays.asList(
                 Mods.TFC_DECORATION.ID,
-                Mods.FORESTRY.ID
-        );
+                Mods.FORESTRY.ID);
     }
 
     @Override
     public void registerForestryRecipes(FMLPostInitializationEvent r) {
         for (Rock rock : TFCRegistries.ROCKS) {
             ForestryRecipeManager.addMoistenerRecipe(
-                    RecipeHelper.getItemStack(Mods.TERRAFIRMACRAFT.ID, "bricks/" + rock.getRegistryName().getPath().toLowerCase()),
-                    RecipeHelper.getItemStack(Mods.TFC_DECORATION.ID, "mossy_bricks/" + rock.getRegistryName().getPath().toLowerCase()),
-                    8
-            );
+                    RecipeHelper.getItemStack(Mods.TERRAFIRMACRAFT.ID,
+                            "bricks/" + rock.getRegistryName().getPath().toLowerCase()),
+                    RecipeHelper.getItemStack(Mods.TFC_DECORATION.ID,
+                            "mossy_bricks/" + rock.getRegistryName().getPath().toLowerCase()),
+                    8);
         }
     }
 }
